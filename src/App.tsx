becdbf0644fbcd62b372042839a3d248d4309858
hairethan12 <153117354+hairethan12@users.noreply.github.com>
2025-03-14 19:04:10 -0600

    Add description

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import BowlerList from "./BowlerList";
+import Description from "./Description";
 
 function App() {
   return (
     <>
+      <Description />
       <BowlerList />
     </>
   );
---
+++ src/Description.tsx
@@ -0,0 +1,9 @@
+function Description() {
+  return (
+    <>
+      <h3>This is a list of Bowlers from the the Sharks and Marlins</h3>
+    </>
+  );
+}
+
+export default Description;
